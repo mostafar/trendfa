9f4f10e5040a296958e87e9872045ac084e2801f
Mostafa Rokooie <mostafa.rokooie@gmail.com>
2017-05-03 23:03:45 -0700

    Database port fix

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from models import Base
 DOCKER_HOST = 'mysql'
 
 # CONNECTION_STRING = 'sqlite:///trendfa.db'
-CONNECTION_STRING = 'mysql+pymysql://root:toor@{}:13306/trendfa'.format(DOCKER_HOST)
+CONNECTION_STRING = 'mysql+pymysql://root:toor@{}/trendfa'.format(DOCKER_HOST)
 
 engine = create_engine(CONNECTION_STRING)
 
