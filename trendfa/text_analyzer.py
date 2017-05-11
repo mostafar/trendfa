ffc3c2aa5fb63ac16c1e4d857e2b6a7370c144ee
Mostafa <mostafa.rokooie@gmail.com>
2017-05-11 04:35:01 +0000

    extract hashtags separately

--- trendfa/text_analyzer.py
+++ trendfa/text_analyzer.py
@@ -14,17 +14,23 @@ def is_word_ok(word):
     return len(word) >= 3 and word not in BLACK_LIST
 
 
+def get_hash_tags(text):
+    return set([word for word in text.strip().split() if word.strip().startswith('#')])
+
+
 def get_names(text):
     tagged_words = tagger.tag(word_tokenize(text))
-    return list(filter(
+    words = set(filter(
         lambda word: is_word_ok(word),
         [tagged_word[0] for tagged_word in filter(lambda tagged_word: tagged_word[1] == 'N', tagged_words)]
     ))
+
+    return words.union(get_hash_tags(text))
 
 
 if __name__ == '__main__':
     print(
         get_names(
-           'حس می کنم'
+            'سلام #ازسندیکابگو'
         )
     )
